d4a8a8dfd5e974556a9730d434b9123f4203e6ff
Felipe300 <caracoloro@gmail.com>
2022-05-01 22:36:46 -0400

    feat: Add public folder to test routes

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import express from 'express'
 import 'dotenv/config'
+import cookieParser from 'cookie-parser'
 
 import './config/connectDB.js'
 import authRouter from './routes/auth.route.js'
 
 const app = express()
-const port = process.env.PORT || 5000
 const apiVersion = '/api/v1'
 
+// only for development testing
+app.use(express.static('public'))
 app.use(express.json())
+app.use(cookieParser())
+
 app.use(`${apiVersion}/auth`, authRouter)
 
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port} 🔥`)
 })
